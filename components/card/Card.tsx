dcf13ee063c98c82eafb29eec6c2f9957dee8411
datteroandrea <dattero.andrea@gmail.com>
2023-01-04 17:00:18 +0100

    major UI improvements

---
+++ components/card/Card.tsx
@@ -0,0 +1,16 @@
+import styles from "./Card.module.css";
+
+export default function Card(props: any) {
+    return <section className={styles.card}>
+        <p className={styles.cardTitle}>{props.title}</p>
+        <div className={styles.cardOffer}>
+        {
+            props.offer?.map((off: any) => {
+                return <p className={styles.cardOfferText}>{off}</p>;
+            })
+        }
+        </div>
+        {props.children}
+        <button className={styles.cardButton}>{props.price}</button>
+    </section>;
+}
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,10 +7,10 @@ export default function Header(props: any) {
     return <header className={styles.header}>
         <Logo />
         <div className={styles.items}>
-            <a>About</a>
-            <a>Services</a>
-            <a>Projects</a>
-            <a>Contact</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
             <Link className={styles.button} href="/resume.pdf">Resume</Link>
         </div>
     </header>;
